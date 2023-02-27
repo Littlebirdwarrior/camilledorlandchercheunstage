cad8a093d94cf357cdd4ddfcc909a55f2add5dc6
Camille Dorland <82466002+Littlebirdwarrior@users.noreply.github.com>
2023-02-27 21:05:23 +0100

    fonction jeu ok - reste visuel a améliorer et score à afficher

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -1,6 +1,68 @@
 //mon script
 
-//Bouger la stagiaire
-$(document).on('mousemove', function(event) {
-    camille.css('left', event.pageX);
+//move Camille with movemove
+$(document).on('mousemove', function (event) {
+   camille.css('left', event.pageX);
 });
+
+
+//falling chocolat infinitely
+function choco_down(chocolats){
+    choco_current_position = parseInt(chocolats.css('top'));
+    chocolats.css('top', choco_current_position + speed) //here, we can update the choco falling speed (cf update score)
+}
+
+//stop chocolat on the ground and return in initial position
+function check_choco_hits_floor(chocolats) {
+    if(collision(chocolats, ground)){
+        decrement_life();
+        return true;
+    }
+    return false;
+}
+
+function set_choco_to_initial_position(chocolats){
+    chocolats.css('top', choco_initial_position);
+}
+
+
+//decrement life when chocolats hit the ground
+function decrement_life(){
+    life--;
+    // life_span.text(life);
+}
+
+//manage when the choco hit camille
+function check_choco_hits_camille(chocolats){
+    if(collision(chocolats, camille)) { //when eggs hit basket, it return to it's inital position
+        //condition, if the basket start is not hitten by the egg top, it is a loss
+        choco_top = parseInt(camille.css('top'));
+        if(choco_top < camille_top) {
+            update_score();
+            return true;
+        }
+    }
+    return false
+}
+
+//update score when the choco hit camille
+function update_score(){
+    score++;
+    //if you play longer, increase difficulty by incrasing speed and set a maximal speed (dont break count function)
+    if(score % 10 === 0 && speed <= max_speed) {
+        speed ++;
+    }
+    // score_span.text(score);
+    // score_1.text(score);
+}
+
+//End the game when loose (cf fonction.js)
+function stop_the_game(){
+    cancelAnimationFrame(anim_id);
+    restart.slideDown();
+}
+
+restart.click(function (){
+    location.reload();
+});
+
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,27 +1,39 @@
-let camille = $('#camille'),
-    gameGround = $('#gameGround'),
-    chocolats = $('.chocolats'),
-    egg1 = $('#choco-1'),
-    egg2 = $('#choco-2'),
-    egg3 = $('#choco-3'),
-    restart = $('#restart'),
-    score_span = $('#score'),
-    score_1 = $('#score_1'),
-    life_span = $('#life'),
-    ground = $('#ground'),
-    camille_height = camille.height(),
-    gameGround_height = gameGround.height(),
-    chocolats_height = chocolats.height(),
-    chocolats_initial_position = parseInt(chocolats.css('top')),
-    score = 0,
-    life = 5,
-    speed = 2,
-    max_speed = 15,
-    counter = 0,
-    score_updated = false,
-    the_game = 0,
-    anim_id = 0,
-    choco_current_position = 0,
-    choco_top = 0,
-    camille_top = container_height - basket_height;
-life_span.text(life);
+$(function () {
+    let the_game = function (){
+        //******choco1
+        //since the choco1 don't hit the ground or camille, he is falling down
+        if (check_choco_hits_floor(choco1) || check_choco_hits_camille(choco1))
+        {
+            set_choco_to_initial_position(choco1);
+        } else {
+            //falling egg infinitly
+            choco_down(choco1);
+        }
+
+        //******choco2
+        if (check_choco_hits_floor(choco2) || check_choco_hits_camille(choco2))
+        {
+            set_choco_to_initial_position(choco2);
+        } else {
+            choco_down(choco2);
+        }
+
+        //******choco3
+        if (check_choco_hits_floor(choco3) || check_choco_hits_camille(choco3))
+        {
+            set_choco_to_initial_position(choco3);
+        } else {
+            choco_down(choco3);
+        }
+
+        //******launch and stop **********/
+        //launch the script of the game (only if life greater than 0)
+        if(life > 0){
+            let anim_id = requestAnimationFrame(the_game);
+        } else {
+            stop_the_game();
+        }
+    };
+
+    anim_id = requestAnimationFrame(the_game);
+});
